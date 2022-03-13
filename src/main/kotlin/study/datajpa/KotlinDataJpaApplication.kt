d4334ea3916f2cc84c6e4ea3e6062fcf2a7a5253
kil <godngu@gmail.com>
2022-03-13 13:29:33 +0900

    Initial Commit

---
+++ src/main/kotlin/study/datajpa/KotlinDataJpaApplication.kt
@@ -0,0 +1,11 @@
+package study.datajpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDataJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDataJpaApplication>(*args)
+}
---
+++ src/test/kotlin/study/datajpa/KotlinDataJpaApplicationTests.kt
@@ -0,0 +1,12 @@
+package study.datajpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDataJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
