940703e3d86b7e3b7bf68687dfa7e85d2a851ee4
kil <godngu@gmail.com>
2022-03-17 07:04:12 +0900

    순수 JPA 기반 레파지토리 만들기

--- src/test/kotlin/study/datajpa/repository/MemberJpaRepositoryTest.kt
+++ src/test/kotlin/study/datajpa/repository/MemberJpaRepositoryTest.kt
@@ -24,4 +24,30 @@ internal class MemberJpaRepositoryTest(@Autowired val memberJpaRepository: Membe
         assertThat(findMember.username).isEqualTo(member.username)
         assertThat(findMember).isEqualTo(member)
     }
+
+    @Test
+    internal fun basicCRUD() {
+        val member1 = Member("member1", 10)
+        val member2 = Member("member2", 20)
+        memberJpaRepository.save(member1)
+        memberJpaRepository.save(member2)
+
+        val findMember1 = memberJpaRepository.findById(member1.id).get()
+        val findMember2 = memberJpaRepository.findById(member2.id).get()
+        assertThat(findMember1).isEqualTo(member1)
+        assertThat(findMember2).isEqualTo(member2)
+
+        val all = memberJpaRepository.findAll()
+        assertThat(all.size).isEqualTo(2)
+        assertThat(all).hasSize(2)
+
+        val count = memberJpaRepository.count()
+        assertThat(count).isEqualTo(2)
+
+        memberJpaRepository.delete(member1)
+        memberJpaRepository.delete(member2)
+
+        val deletedCount = memberJpaRepository.count()
+        assertThat(deletedCount).isEqualTo(0)
+    }
 }
